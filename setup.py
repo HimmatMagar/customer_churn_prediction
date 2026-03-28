e6ca90cc9d32851ed6bb0c99b8f70ff4cf723d4b
Himmat Magar <himmatmagar007@gmail.com>
2026-03-28 19:03:31 +0545

    experiment ended

--- setup.py
+++ setup.py
@@ -4,16 +4,15 @@ __version__ = '0.0.0.0'
 
 REPO_NAME = "customer_churn_prediction"
 AUTHOR_USER_NAME = "HimmatMagar"
-SRC_REPO = "churn-prediction"
+SRC_REPO = "churn_prediction"
 AUTHOR_EMAIL = "himmatmagar007@gmail.com"
 
 setuptools.setup(
-      name="churn-prediction",
+      name="churn_prediction",
       version=__version__,
       author=AUTHOR_USER_NAME,
       author_email=AUTHOR_EMAIL,
       description="End to End ML Learning implementation for customer churn prediction",
-      long_description_content_type='text/markdown',
       url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
       project_urls={
             "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issue"
--- src/churn_prediction/__init__.py
+++ src/churn_prediction/__init__.py
@@ -4,15 +4,16 @@ import logging
 
 log_dir = "Logging"
 log_file = os.path.join(log_dir, "running_log.log")
-os.makedirs(log_dir, exist_ok = True)
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
-      level = logging.INFO,
-      format = "[%(asctime)s: %(level)s: %(module)s: %(message)s]",
-      handlers=[
-            logging.FileHandler(log_dir),
-            logging.StreamHandler(sys.stdout(log_file))
-      ]
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]",
+    handlers=[
+        logging.FileHandler(log_file),
+        logging.StreamHandler(sys.stdout)
+    ]
 )
+
 
 logger = logging.getLogger("churn-prediction")
